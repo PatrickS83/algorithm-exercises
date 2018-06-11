f58d5463d3b7adcbd470f224fea5675fbd8db560
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-11 15:14:08 +0200

    committed before saving ....

---
+++ freecodecamp/basic/boo-who.js
@@ -0,0 +1,8 @@
+// Basic Algorithm Scripting: Boo who
+// Check if a value is classified as a boolean primitive. Return true or false.
+
+function booWho(bool) {
+  return typeof bool === 'boolean';
+}
+
+booWho(null);
