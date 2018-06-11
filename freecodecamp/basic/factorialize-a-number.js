cd14cee602379ea350d6efaeeab08d8c416d446f
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-11 14:25:23 +0200

    cretad FCC algorithm for "Factorialize a Number" challenge

---
+++ freecodecamp/basic/factorialize-a-number.js
@@ -0,0 +1,12 @@
+// Basic Algorithm Scripting: Factorialize a Number
+// Return the factorial of the provided integer.
+
+function factorialize(num) {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+factorialize(5);
