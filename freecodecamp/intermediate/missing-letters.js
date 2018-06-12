0140d2c37860230d658a60fc1d023c1f60ee8472
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-12 13:52:23 +0200

    minor refactor: fixed variable shadowing

--- freecodecamp/intermediate/missing-letters.js
+++ freecodecamp/intermediate/missing-letters.js
@@ -7,7 +7,7 @@ function fearNotLetter(str) {
   const charCode = str
     .split('')
     .map(letter => letter.charCodeAt(0))
-    .find((charCode, i, arr) => charCode + 1 !== arr[i + 1])
+    .find((code, i, arr) => code + 1 !== arr[i + 1])
 
   if (charCode === str.charCodeAt(str.length - 1)) return undefined; // no missing letter
   return String.fromCharCode(charCode + 1);
