aeaf427035f3e2558914f93ca5fb178d381fd830
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-12 14:47:47 +0200

    solved fcc challenge "sum odd fibonaccis"

---
+++ freecodecamp/intermediate/sum-odd-fibbonacis.js
@@ -0,0 +1,26 @@
+// Intermediate Algorithm Scripting: Sum All Odd Fibonacci Numbers
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+
+// The first two numbers in the Fibonacci sequence are 1 and 1.
+// Every additional number in the sequence is the sum of the two previous numbers.
+// The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+
+// For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+
+function sumFibs(num) {
+  let result = 2; // first two fibonaccis already summed
+
+  const sumOdd = (fibo) => {
+    const summedFibo = fibo[0] + fibo[1];
+
+    if (summedFibo <= num) sumOdd([fibo[1], summedFibo])
+    else return
+
+    if (summedFibo % 2 !== 0) result += summedFibo
+  }
+
+  sumOdd([1, 1])
+  return result;
+}
+
+sumFibs(1000);
