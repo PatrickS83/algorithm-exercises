f9e5ba7f93e23b8a644ffca62251ebcbe822658c
PatrickSchuelke <p-schuelke@gmx.de>
2018-06-24 21:46:11 +0200

    completed codewars challenge "math issues"

---
+++ codewars/math-issues-(kyu5).js
@@ -0,0 +1,15 @@
+// Oh no, our Math object was "accidently" reset. Can you re-implement some of those functions?
+// We can assure, that only non-negative numbers are passed as arguments.
+// So you don't have to consider things like undefined, null, NaN, negative numbers, strings and so on.
+
+// Here is a list of functions, we need:
+
+// Math.round()
+// Math.ceil()
+// Math.floor()
+
+Math.ceil = number => (Number.isInteger(number) ? number : parseInt(number + 1, 10));
+
+Math.floor = number => (Number.isInteger(number) ? number : parseInt(number, 10));
+
+Math.round = number => (number % 1 < 0.5 ? Math.floor(number) : Math.ceil(number));
